bb90ae1cc63ce838955a17c007c92369c5384628
Sam Benemerito <benemeritosam@gmail.com>
2019-11-19 11:40:14 +0900

    initial location feature

--- mobile/App.js
+++ mobile/App.js
@@ -1,12 +1,50 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import * as Location from 'expo-location';
+import * as Permissions from 'expo-permissions';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
+export default class App extends React.Component {
+  state = {
+    location: null
+  }
+
+  componentDidMount() {
+    this._getLocationPermission();
+    Location.watchPositionAsync({
+      accuracy: Location.Accuracy.Balanced,
+      timeInterval: 1000,
+      distanceInterval: 1,
+    }, location => {
+      this.setState({ location });
+      console.log(location, 'location');
+    });
+  }
+
+  render() {
+    const { location } = this.state;
+
+    return (
+      <View style={styles.container}>
+        <Text>
+        {
+          location ? `Latitiude: ${location.coords.latitude}\n
+                      Longitude: ${location.coords.longitude}\n
+                      Speed: ${location.coords.speed * 3.6} KPH`
+                   : 'null'
+        }
+        </Text>
+      </View>
+    );
+  }
+
+  _getLocationPermission = async () => {
+    let { status } = await Permissions.askAsync(Permissions.LOCATION);
+    if (status !== 'granted') {
+      this.setState({
+        errorMessage: 'Permission to access location was denied',
+      });
+    }
+  };
 }
 
 const styles = StyleSheet.create({
